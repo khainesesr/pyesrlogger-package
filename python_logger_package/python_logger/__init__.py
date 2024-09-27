06add1eba14f128f0087609fa36f2e3f27746d23
khainesesr <102272560+khainesesr@users.noreply.github.com>
2024-09-27 13:17:45 +1200

    Add files via upload

---
+++ python_logger_package/python_logger/__init__.py
@@ -0,0 +1,1 @@
+from .python_error_logging import JobHandler
---
+++ python_logger_package/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python_logger_package',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple Python package that includes a logging decorator.',
+    author='Kaitlin',
+    author_email='kaitlin.haines@esr.cri.nz',
+    #url='https://github.com/yourusername/my_decorator_package',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
