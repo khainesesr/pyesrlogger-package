dd989220ec6cc423a600092434830f16a987b190
khainesesr <102272560+khainesesr@users.noreply.github.com>
2024-09-27 14:00:29 +1200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='python_logger_package',
+    name='pyesrlogger',
     version='0.1',
     packages=find_packages(),
     description='A simple Python package that includes a logging decorator.',
