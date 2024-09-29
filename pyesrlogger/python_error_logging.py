1962b826a06cc540e27bd372dd0313454cd263ce
khainesesr <102272560+khainesesr@users.noreply.github.com>
2024-09-29 19:24:59 +1300

    Update python_error_logging.py

--- pyesrlogger/python_error_logging.py
+++ pyesrlogger/python_error_logging.py
@@ -13,7 +13,7 @@ from sqlalchemy import create_engine
 import dotenv
 import traceback
 import warnings
-from decorators import env_variable_check
+from .decorators import env_variable_check
 Renvconfigfile='../.Renviron'
 dotenv.load_dotenv(Renvconfigfile)
 
