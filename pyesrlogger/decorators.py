b62d724b343714d6c9031ca00058d4b0aa4ff092
khainesesr <102272560+khainesesr@users.noreply.github.com>
2024-09-29 19:50:44 +1300

    Update decorators.py

--- pyesrlogger/decorators.py
+++ pyesrlogger/decorators.py
@@ -1,1 +1,12 @@
+import os
+from dotenv import load_dotenv
 
+def load_env_files(*env_files):
+    """Decorator to load multiple environment variable files."""
+    def decorator(cls):
+        def wrapper(*args, **kwargs):
+            for env_file in env_files:
+                load_dotenv(env_file)
+            return cls(*args, **kwargs)  # Instantiate the class
+        return wrapper
+    return decorator
