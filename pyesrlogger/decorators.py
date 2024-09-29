049f335da35a4729025349e9b3494d3a082db410
khainesesr <102272560+khainesesr@users.noreply.github.com>
2024-09-29 19:39:37 +1300

    Update decorators.py

--- pyesrlogger/decorators.py
+++ pyesrlogger/decorators.py
@@ -1,11 +1,1 @@
-import os
 
-def env_variable_check(key):
-    """Decorator to check if an environment variable is set."""
-    def decorator(func):
-        def wrapper(self, *args, **kwargs):
-            if key not in os.environ:
-                raise KeyError(f"Environment variable '{key}' not found in environment file.")
-            return func(self, *args, **kwargs)  # Call the original __init__ method
-        return wrapper
-    return decorator
