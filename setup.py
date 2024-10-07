4e3388ed863b8488080c90aeaca6e650508d295e
khainesesr <102272560+khainesesr@users.noreply.github.com>
2024-10-07 15:26:56 +1300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name='pyesrlogger',
     version='0.1',
     packages=find_packages(),
-    install_requires=['pyodbc','sqlalchemy','pandas','python-dotenv'],
+    install_requires=['pyodbc','sqlalchemy','pandas','python-dotenv', \
+                     'envdecorator-package @ git+https://github.com/khainesesr/envdecorator-package#egg=envdecorator-package'],
     description='A simple Python package that includes a logging decorator.',
     author='Kaitlin',
     author_email='kaitlin.haines@esr.cri.nz',
